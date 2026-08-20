cbe96232736261a1444645552313de1b78c7b79a
Ethan <122490037+KodaNotABear@users.noreply.github.com>
2026-08-20 02:25:07 -0700

    Initial Commit

---
+++ src/main/java/studio/akuro/arsqol/ArsQol.java
@@ -0,0 +1,14 @@
+package studio.akuro.arsqol;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ArsQol.MOD_ID)
+public class ArsQol {
+    public static final String MOD_ID = "ars_qol";
+
+    public ArsQol(ModContainer container, IEventBus modBus) {
+    }
+
+}
